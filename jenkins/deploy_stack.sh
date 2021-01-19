bf8e066c4ee79931de0706826aa40c7fb8191ce6
htr-volker <htr.volker@gmail.com>
2021-01-19 18:53:14 +0000

    setting secret key in swarm ssh

--- jenkins/deploy_stack.sh
+++ jenkins/deploy_stack.sh
@@ -2,5 +2,6 @@
 scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-master << EOF
     export DATABASE_URI=${DATABASE_URI}
+    export SECRET_KEY=${SECRET_KEY}
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml todo-stack
 EOF
