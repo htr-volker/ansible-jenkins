113883126bb2f8a3be0ae6fff55ae8964c04ee76
htr-volker <htr.volker@gmail.com>
2021-01-19 18:46:57 +0000

    fixing scp command to allow compose.yaml to be copied to manager

--- jenkins/deploy_stack.sh
+++ jenkins/deploy_stack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-scp docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-master << EOF
     export DATABASE_URI=${DATABASE_URI}
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml todo-stack
