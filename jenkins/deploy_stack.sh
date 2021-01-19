1ad376c23e40377d053ab5f3fc8fecdd0df4dd2a
htr-volker <htr.volker@gmail.com>
2021-01-19 18:45:10 +0000

    fixing docker stack deploy command

--- jenkins/deploy_stack.sh
+++ jenkins/deploy_stack.sh
@@ -2,5 +2,5 @@
 scp docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-master << EOF
     export DATABASE_URI=${DATABASE_URI}
-    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml
+    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml todo-stack
 EOF
